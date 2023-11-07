b4ae638cbd5e104dc9087d6c9db76b9f7ffb8a78
percy <percy.u.lomas@gmail.com>
2023-11-06 19:24:10 -0500

    Revert "finish-capituo8"
    This reverts commit 260db2f816606f81f1b24b8d7a9bd6315966bedf.

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -9,22 +9,21 @@ import {
   Revenue,
 } from './definitions';
 import { formatCurrency } from './utils';
-export const dynamic = "force-dynamic"
-// import { unstable_noStore as noStore } from 'next/cache';
-
 
 export async function fetchRevenue() {
   // Add //noStore() here prevent the response from being cached.
   // This is equivalent to in fetch(..., {cache: 'no-store'}).
-  //noStore();
+
   try {
     // Artificially delay a reponse for demo purposes.
     // Don't do this in real life :)
 
-    await new Promise((resolve) => setTimeout(resolve, 1000));
+    // console.log('Fetching revenue data...');
+    // await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
+    // console.log('Data fetch complete after 3 seconds.');
 
     return data.rows;
   } catch (error) {
