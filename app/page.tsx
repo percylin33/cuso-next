3b6f22b814de00dc41ea2fd0d22e32713aa0467d
percy <percy.u.lomas@gmail.com>
2023-11-11 02:59:24 -0500

    meta-data-capitulo-16

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,14 @@ import { lusitana } from './ui/fonts';
 import Image from 'next/image';
 
 import Link from 'next/link';
+
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: 'Acme Dashboard',
+  description: 'The official Next.js Course Dashboard, built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
