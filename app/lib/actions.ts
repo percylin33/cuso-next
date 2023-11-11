9936dc339d420b69a54dd44489eb8d2d6eed0706
percy <percy.u.lomas@gmail.com>
2023-11-11 01:05:52 -0500

    nextAuth-capitulo-15

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,7 @@ import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { signIn } from '@/auth';
 
 const InvoiceSchema = z.object({
     id: z.string(),
@@ -114,4 +115,18 @@ export async function updateInvoice(
       }
     }
     revalidatePath('/dashboard/invoices');
+  }
+
+  export async function authenticate(
+    prevState: string | undefined,
+    formData: FormData,
+  ) {
+    try {
+      await signIn('credentials', Object.fromEntries(formData));
+    } catch (error) {
+      if ((error as Error).message.includes('CredentialsSignin')) {
+        return 'CredentialSignin';
+      }
+      throw error;
+    }
   }
