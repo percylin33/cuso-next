ea7768305caacdfe6109f146a5fc46961bea417a
percy <percy.u.lomas@gmail.com>
2023-11-11 17:11:45 -0500

    log

--- auth.config.ts
+++ auth.config.ts
@@ -9,6 +9,8 @@ export const authConfig = {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
       const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
+      console.log(isOnDashboard);
+      
       if (isOnDashboard) {
         if (isLoggedIn) return true;
         return false; // Redirect unauthenticated users to login page
