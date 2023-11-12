cae69e484f070be51a7a39ed5a04818e1b36b5fd
percy <percy.u.lomas@gmail.com>
2023-11-11 21:41:02 -0500

    mas-1-image-tablle

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -79,6 +79,13 @@ export default async function InvoicesTable({
               </tr>
             </thead>
             <tbody className="bg-white">
+            <Image
+                        src={invoices[0].image_url}
+                        className="rounded-full"
+                        width={28}
+                        height={28}
+                        alt={`${invoices[0].name}'s profile picture`}
+                      />
               {invoices?.map((invoice) => (
                 <tr
                   key={invoice.id}
