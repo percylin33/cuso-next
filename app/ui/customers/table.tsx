b3c236dd357363a0f6834414ff0bb693e6ea8515
percy <percy.u.lomas@gmail.com>
2023-11-17 06:26:22 -0500

    public

--- app/ui/customers/table.tsx
+++ app/ui/customers/table.tsx
@@ -29,7 +29,7 @@ export default async function CustomersTable({
                         <div className="mb-2 flex items-center">
                           <div className="flex items-center gap-3">
                             <Image
-                              src={customer.image_url}
+                              src={`/public${customer.image_url}`}
                               className="rounded-full"
                               alt={`${customer.name}'s profile picture`}
                               width={28}
