65cc6300665f22ca52e9628016db8f8fcd7a59f0
percy <percy.u.lomas@gmail.com>
2023-11-16 19:43:31 -0500

    credenciales

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ export const metadata: Metadata = {
   metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
 };
 export default function Page() {
-  return (
+  return (     
     <main className="flex min-h-screen flex-col p-6">
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
         <AcmeLogo />
@@ -40,19 +40,19 @@ export default function Page() {
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
           <Image
-        src="/hero-desktop.png"
-        width={1000}
-        height={760}
-        className="hidden md:block"
-        alt="Screenshots of the dashboard project showing desktop and mobile versions"
-      />
-      <Image
-        src="/hero-mobile.png"
-        width={560}
-        height={620}
-        className="block md:hidden"
-        alt="Screenshot of the dashboard project showing mobile version"
-      />
+             src="/hero-desktop.png"
+             width={1000}
+             height={760}
+             className="hidden md:block"
+             alt="Screenshots of the dashboard project showing desktop and mobile versions"
+           />
+           <Image
+             src="/hero-mobile.png"
+             width={560}
+             height={620}
+             className="block md:hidden"
+             alt="Screenshot of the dashboard project showing mobile version"
+           />
 
         </div>
       </div>
--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -17,7 +17,9 @@ export default function LoginForm() {
     <form action={action}  className="space-y-3">
       <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
         <h1 className={`${lusitana.className} mb-3 text-2xl`}>
-          Please log in to continue.
+          Please log in to continue using. 
+          <p className='text-lg'>Email: user@nextmail.com</p>
+          <p className='text-lg'>Password: 123456</p>
         </h1>
         <div className="w-full">
           <div>
